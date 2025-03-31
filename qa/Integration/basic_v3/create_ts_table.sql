3bf55c2701096652b47771cb1e222f38de06e1c2
Yongyan Wang <wangyongyan@inspur.com>
2025-03-31 00:40:20 +0000

    feat: Added first regression test for V3.0. (ZDP-47360)

--- qa/Integration/basic_v3/create_ts_table.sql
+++ qa/Integration/basic_v3/create_ts_table.sql
@@ -1,4 +1,5 @@
 create ts database test;
 create table test.sjcx01(k_timestamp timestamp not null,A INT4 not null,B INT4 not null,C VARCHAR(10) not null,D VARCHAR(10) not null)attributes (t1_attribute varchar not null) primary tags(t1_attribute);
 insert into test.sjcx01 values (1681111110000,1,1,'A','DFFDARG', 'qqq');
-insert into test.sjcx01 values (1681111110001,2,2,'B','DFFDARGD', 'qqq1');
+select * from test.sjcx01 order by k_timestamp;
+drop database test cascade;
