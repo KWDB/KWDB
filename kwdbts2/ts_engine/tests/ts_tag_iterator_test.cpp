cecc8fa427d8d11a7fe404cfdcd4bacf4f2de2a3
Yaxin Liu <liuyaxin01@inspur.com>
2025-04-04 05:08:31 +0000

    merge st-v3 and resolve conflicts, ZDP-47363

--- kwdbts2/ts_engine/tests/ts_tag_iterator_test.cpp
+++ kwdbts2/ts_engine/tests/ts_tag_iterator_test.cpp
@@ -80,7 +80,10 @@ TEST_F(TestEngine, tagiterator) {
   data_value = GenRowPayload(metric_schema, tag_schema ,cur_table_id, 1, start_ts1, 1, start_ts1);
 
   TSTableID ts_id(cur_table_id);
-  s = ts_engine_->PutData(ctx_, ts_id, 1, &data_value, true);
+  uint16_t inc_entity_cnt;
+  uint32_t inc_unordered_cnt;
+  DedupResult dedup_result{0, 0, 0, TSSlice {nullptr, 0}};
+  s = ts_engine_->PutData(ctx_, ts_id, 0, &data_value, 1, 0, &inc_entity_cnt, &inc_unordered_cnt, &dedup_result, true);
   ASSERT_EQ(s , KStatus::SUCCESS);
   free(data_value.data);
 
