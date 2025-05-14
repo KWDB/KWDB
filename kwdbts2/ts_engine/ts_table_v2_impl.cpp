4326775052da6148892b6071b02a435b195c3b0a
qinlipeng <qinlipeng@inspur.com>
2025-05-14 10:59:40 +0800

    fix: lifetime

--- kwdbts2/ts_engine/ts_table_v2_impl.cpp
+++ kwdbts2/ts_engine/ts_table_v2_impl.cpp
@@ -62,7 +62,7 @@ KStatus TsTableV2Impl::PutData(kwdbContext_p ctx, TsVGroup* v_group, TsRawPayloa
   // The time is seconds level.
   int64_t acceptable_ts = INT64_MIN;
   auto life_time = GetLifeTime();
-  if (life_time != UINT64_MAX) {
+  if (life_time != 0) {
     auto now = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
     acceptable_ts = now.time_since_epoch().count() - life_time;
   }
