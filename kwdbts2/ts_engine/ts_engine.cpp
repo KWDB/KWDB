75ef442b89b7ab303d40d03419933cf6d3202069
Zin <xinyue@inspur.com>
2025-07-17 13:40:08 +0800

    fix skip error

--- kwdbts2/ts_engine/ts_engine.cpp
+++ kwdbts2/ts_engine/ts_engine.cpp
@@ -669,7 +669,7 @@ KStatus TSEngineV2Impl::TSMtrRollback(kwdbContext_p ctx, const KTableKey& table_
   }
   KStatus s;
 
-  if (skip_log) {
+  if (!skip_log) {
     auto vgroup = GetVGroupByID(ctx, 1);
     s = vgroup->MtrRollback(ctx, mtr_id);
     if (s == FAIL) {
