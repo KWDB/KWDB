f9b2bf10779df8e5ce382894f5f3d75c7bb49b04
Zin <xinyue@inspur.com>
2025-07-17 11:22:46 +0800

    fix lint error

--- kwdbts2/ts_engine/ts_engine.cpp
+++ kwdbts2/ts_engine/ts_engine.cpp
@@ -705,7 +705,7 @@ KStatus TSEngineV2Impl::TSMtrRollback(kwdbContext_p ctx, const KTableKey& table_
   std::reverse(rollback_logs.begin(), rollback_logs.end());
   for (auto log : rollback_logs) {
     auto vgrp_id = log->getVGroupID();
-    // todo(xy), 0 means the wal log does not need to rollback. 
+    // todo(xy), 0 means the wal log does not need to rollback.
     if (vgrp_id == 0) {
       continue;
     }
