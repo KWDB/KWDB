5653566f6ba07d37718d7333e105295f74ca326f
Yongyan Wang <wangyongyan@inspur.com>
2025-08-06 16:00:42 +0000

    fix: Fixed compilation errors.

--- kwdbts2/ts_engine/include/ts_block.h
+++ kwdbts2/ts_engine/include/ts_block.h
@@ -133,21 +133,21 @@ class TsBlockSpan {
   // if just get timestamp, these function return fast.
   void GetTSRange(timestamp64* min_ts, timestamp64* max_ts);
 
-  bool IsColExist(uint32_t scan_idx) { return convert_.IsColExist(scan_idx); }
-  bool IsColNotNull(uint32_t scan_idx) { return convert_.IsColNotNull(scan_idx); }
-  bool IsSameType(uint32_t scan_idx) { return convert_.IsSameType(scan_idx); }
-  bool IsVarLenType(uint32_t scan_idx) { return convert_.IsVarLenType(scan_idx); }
-  int32_t GetColSize(uint32_t scan_idx) { return convert_.GetColSize(scan_idx); }
-  int32_t GetColType(uint32_t scan_idx) { return convert_.GetColType(scan_idx); }
-  KStatus GetColBitmap(uint32_t scan_idx, TsBitmap& bitmap) { return convert_.GetColBitmap(scan_idx, bitmap); }
+  bool IsColExist(uint32_t scan_idx) { return convert_->IsColExist(scan_idx); }
+  bool IsColNotNull(uint32_t scan_idx) { return convert_->IsColNotNull(scan_idx); }
+  bool IsSameType(uint32_t scan_idx) { return convert_->IsSameType(scan_idx); }
+  bool IsVarLenType(uint32_t scan_idx) { return convert_->IsVarLenType(scan_idx); }
+  int32_t GetColSize(uint32_t scan_idx) { return convert_->GetColSize(scan_idx); }
+  int32_t GetColType(uint32_t scan_idx) { return convert_->GetColType(scan_idx); }
+  KStatus GetColBitmap(uint32_t scan_idx, TsBitmap& bitmap) { return convert_->GetColBitmap(scan_idx, bitmap); }
 
   // dest type is fixed len datatype.
   KStatus GetFixLenColAddr(uint32_t scan_idx, char** value, TsBitmap& bitmap, bool bitmap_required = true) {
-    return convert_GetFixLenColAddr(scan_idx, value, bitmap, bitmap_required);
+    return convert_->GetFixLenColAddr(scan_idx, value, bitmap, bitmap_required);
   }
   // dest type is varlen datatype.
   KStatus GetVarLenTypeColAddr(uint32_t row_idx, uint32_t scan_idx, DataFlags& flag, TSSlice& data) {
-    return convert_.GetVarLenTypeColAddr(row_idx, scan_idx, flag, data);
+    return convert_->GetVarLenTypeColAddr(row_idx, scan_idx, flag, data);
   }
 
   KStatus GetCount(uint32_t scan_idx, uint32_t& count);
