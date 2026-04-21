1270e6468dfb82a1d5c4008708bcd747bb7041b7
qiliang <qiliang01@inspur.com>
2026-04-21 02:30:59 +0000

    update license

--- kwbase/pkg/sql/sem/builtins/pg_builtins_test.go
+++ kwbase/pkg/sql/sem/builtins/pg_builtins_test.go
@@ -1,16 +1,13 @@
-//
 // Copyright (c) 2022-present, Shanghai Yunxi Technology Co, Ltd. All rights reserved.
 //
-// This software is the confidential and proprietary information of Shanghai Yunxi Technology Co, Ltd.
-// You shall not disclose such confidential information and shall use it only in accordance with
-// the terms of the license agreement you entered into with Shanghai Yunxi Technology Co, Ltd.
-//
-// Shanghai Yunxi Technology Co, Ltd makes no representations or warranties about the suitability
-// of the software, either express or implied, including but not limited to the implied warranties
-// of merchantability, fitness for a particular purpose, or non-infringement. Shanghai Yunxi
-// Technology Co, Ltd shall not be liable for any damages suffered by licensee as a result
-// of using, modifying or distributing this software or its derivatives.
-//
+// This software (KWDB) is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
 
 package builtins
 
